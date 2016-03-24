8851f453b4d2058713e1b60a9ac3f05009a55ba2
Angel Thomas <athomas@bex.net>
2016-03-24 13:19:04 -0400

    Added if statement to check for character

--- src/ArabicNumerals.java
+++ src/ArabicNumerals.java
@@ -3,9 +3,19 @@ public class ArabicNumerals {
 
 	public int convertArabic(String roman) {
 		int x = roman.length();
+		int []numArray = new int[roman.length()];
+		for (int i = 0; i < x; i++) {
+			char romanChar = roman.charAt(i);
+			if (romanChar == 'I') {
+				numArray[i] = 1;
+			} else {
+				numArray[i] = 5;
+			}
+		}
+		
 		int total = 0;
 		for (int i = 0; i < x; i++) {
-			total += 1;
+			total += numArray[i];
 		}
 		return total;
 	}
