7bde3cf2283ef92bf10ce8cdbd21dfd1dcefd238
Angel Thomas <athomas@bex.net>
2016-03-24 14:32:40 -0400

    Refactored test class

--- src/RomanNumeralsTest.java
+++ src/RomanNumeralsTest.java
@@ -1,19 +1,24 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class RomanNumeralsTest {
+	private RomanNumerals rn;
+	
+	@Before
+	public void beforeEachTest() {
+		rn = new RomanNumerals();
+	}
 
 	@Test
 	public void inputOf1returnsI() {
-		RomanNumerals rn = new RomanNumerals();
 		assertEquals("I", rn.convertRoman(1));
 	}
-	
+
 	@Test
 	public void inputOf2returnsII() {
-		RomanNumerals rn = new RomanNumerals();
 		assertEquals("II", rn.convertRoman(2));
 	}
 
