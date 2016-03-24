beb1ad8afabc0b1f1efa07554f5584da1a9c9741
Angel Thomas <athomas@bex.net>
2016-03-24 14:36:46 -0400

    Code added to accommodate 4

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -4,8 +4,13 @@ public class RomanNumerals {
 	public String convertRoman(int i) {
 		String roman = "";
 		for (int c = i; c > 0;) {
-			roman += "I";
-			c -= 1;
+			if (c == 4) {
+				roman += "IV";
+				c -= 4;
+			} else {
+			    roman += "I";
+			    c -= 1;
+			}
 		}
 		return roman;
 	}
