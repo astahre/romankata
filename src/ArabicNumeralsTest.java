9913f0114cddcbd366caaada64b42901bba5f236
Angel Thomas <athomas@bex.net>
2016-03-24 12:49:56 -0400

    Refactored test class

--- src/ArabicNumeralsTest.java
+++ src/ArabicNumeralsTest.java
@@ -1,19 +1,24 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class ArabicNumeralsTest {
+	private ArabicNumerals an;
+	
+	@Before
+	public void beforeEachTest() {
+		an = new ArabicNumerals();
+	}
 
 	@Test
 	public void inputofIReturns1() {
-		ArabicNumerals an = new ArabicNumerals();
 		assertEquals(1, an.convertArabic("I"));
 	}
 	
 	@Test
 	public void inputofIIReturns2() {
-		ArabicNumerals an = new ArabicNumerals();
 		assertEquals(2, an.convertArabic("II"));
 	}
 
