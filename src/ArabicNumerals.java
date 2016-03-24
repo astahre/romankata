c60080ddfec4ef723319b49556348c2e70f6b1c0
Angel Thomas <athomas@bex.net>
2016-03-24 12:45:54 -0400

    For loop added to iterate across string

--- src/ArabicNumerals.java
+++ src/ArabicNumerals.java
@@ -1,8 +1,13 @@
 
 public class ArabicNumerals {
 
-	public int convertArabic(String string) {
-		return 1;
+	public int convertArabic(String roman) {
+		int x = roman.length();
+		int total = 0;
+		for (int i = 0; i < x; i++) {
+			total += 1;
+		}
+		return total;
 	}
 
 }
